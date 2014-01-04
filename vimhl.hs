492dec0e693251aae72967154412a9b94ff28be7
Alexey Radkov <alexey.radkov@gmail.com>
2014-01-04 19:27:06 +0400

    version 0.3

--- vimhl.hs
+++ vimhl.hs
@@ -4,6 +4,7 @@
 import Text.Pandoc.JSON
 import System.IO
 import System.Directory
+import System.FilePath
 import System.Process
 
 vimHl :: Maybe Format -> Block -> IO Block
@@ -22,13 +23,31 @@ vimHl (Just format) cb@(CodeBlock (id, classes@(ft:_), namevals) contents)
                                 Nothing  -> " -1"
                                 Just val -> " " ++ val
                             | otherwise = ""
+                colorscheme =
+                    case lookup "colorscheme" namevals of
+                    Nothing -> ""
+                    Just val -> "-c 'let g:PhColorscheme = \"" ++ val ++ "\"' "
+                vimrcM = do
+                    home <- getHomeDirectory
+                    exists <- doesFileExist $ vimrc home
+                    if exists
+                        then do
+                            permissions <- getPermissions $ vimrc home
+                            if readable permissions
+                                then return $ "--noplugin -u '" ++ vimrc home ++
+                                              "' "
+                                else return ""
+                        else return ""
+                    where vimrc home = home `combine` ".vimrc.pandoc"
+            vimrc <- vimrcM
             writeFile tempbuf contents
             {- vim must think that it was launched from a terminal, otherwise
              - it won't load its usual environment and the syntax engine! -}
             hin <- openFile "/dev/tty" ReadMode
             (_, Just hout, _, handle) <- createProcess (shell $
-                "vim -Ne -c 'set ft=" ++ ft ++ " | " ++ vimhlcmd ++ "' " ++
-                "-c 'w! " ++ tempfile ++ "' -c 'qa!' " ++ tempbuf)
+                "vim -Ne " ++ vimrc ++ colorscheme ++ "-c 'set ft=" ++ ft ++
+                " | " ++ vimhlcmd ++ "' " ++ "-c 'w! " ++ tempfile ++
+                "' -c 'qa!' " ++ tempbuf)
                 {std_in = UseHandle hin, std_out = CreatePipe}
             waitForProcess handle
             hClose hin
