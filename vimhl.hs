006f2b5d89d6c0f773045223fc6b8f0af97867ed
Alexey Radkov <alexey.radkov@gmail.com>
2014-01-05 01:54:58 +0400

    case insensitive comparison of namevals

--- vimhl.hs
+++ vimhl.hs
@@ -6,11 +6,12 @@ import System.IO
 import System.Directory
 import System.FilePath
 import System.Process
+import qualified Data.CaseInsensitive as CI
 
 vimHl :: Maybe Format -> Block -> IO Block
 vimHl (Just format) cb@(CodeBlock (id, classes@(ft:_), namevals) contents)
   | format == Format "html" || format == Format "latex" =
-        case lookup "hl" namevals of
+        case lookup (CI.mk "hl") ci_namevals of
         Just "vim" -> do
             let tempbuf  = "_vimhl_buffer"
                 tempfile = "_vimhl_result"
@@ -19,12 +20,12 @@ vimHl (Just format) cb@(CodeBlock (id, classes@(ft:_), namevals) contents)
                     | format == Format "latex" = "MakeTexCodeHighlight" ++ nmb
                     where nmb
                             | "numberLines" `elem` classes =
-                                case lookup "startfrom" namevals of
+                                case lookup (CI.mk "startFrom") ci_namevals of
                                 Nothing  -> " -1"
                                 Just val -> " " ++ val
                             | otherwise = ""
                 colorscheme =
-                    case lookup "colorscheme" namevals of
+                    case lookup (CI.mk "colorScheme") ci_namevals of
                     Nothing -> ""
                     Just val -> "-c 'let g:PhColorscheme = \"" ++ val ++ "\"' "
                 vimrcM = do
@@ -58,6 +59,7 @@ vimHl (Just format) cb@(CodeBlock (id, classes@(ft:_), namevals) contents)
             return $ RawBlock format block
         _          -> return cb
   | otherwise = return cb
+  where ci_namevals = map (\(x, y) -> (CI.mk x, y)) namevals
 vimHl _ cb = return cb
 
 main :: IO ()
