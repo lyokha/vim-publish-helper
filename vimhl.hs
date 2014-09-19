1953e7acb5a1996d7e6f7aed6627eb0f23fbb542
lyokha <alexey.radkov@gmail.com>
2014-09-19 17:12:14 +0400

    stylistic and further hlint improvements

--- vimhl.hs
+++ vimhl.hs
@@ -9,69 +9,68 @@ import System.Directory
 import System.FilePath
 import System.Process
 import Data.Char (toLower)
+import Data.Maybe (fromMaybe)
 import Control.Arrow (first)
 
 vimHl :: Maybe Format -> Block -> IO Block
 vimHl (Just fm@(Format fmt)) cb@(CodeBlock (_, cls@(ft:_), namevals) contents)
-  | lookup "hl" namevals' == Just "vim" && fmt `elem` ["html", "latex"] = do
-      let vimhlcmd
-              | fmt == "html"  = "MakeHtmlCodeHighlight" ++ nmb
-              | fmt == "latex" = "MakeTexCodeHighlight" ++ nmb
-              where nmb
-                      | "numberLines" `elem` cls =
-                          case lookup "startfrom" namevals' of
-                              Nothing  -> " -1"
-                              Just val -> ' ' : val
-                      | otherwise = ""
-          colorscheme =
-              case lookup "colorscheme" namevals' of
-                  Nothing -> ""
-                  Just val -> "-c 'let g:PhColorscheme = \"" ++ val ++ "\"' "
-          cmds =
-              case lookup "vars" namevals' of
-                  Nothing -> ""
-                  Just val ->
-                      unwords (map (cmd . flag) $ filter (not . null) $
-                          map (splitRegex regex') $ splitRegex regex val) ++ " "
-                      where cmd (x:y:_) =
-                                "--cmd 'let g:" ++ x ++ " = \"" ++ y ++ "\"'"
-                            flag [x]    = [x, "1"]
-                            flag x      = x
-                            regex       = mkRegex "[[:space:]]*,[[:space:]]*"
-                            regex'      = mkRegex "[[:space:]]*=[[:space:]]*"
-          vimrcM = do
-              home <- getHomeDirectory
-              let vimrc = home `combine` ".vimrc.pandoc"
-              exists <- doesFileExist vimrc
-              if exists
-                  then do
-                      permissions <- getPermissions vimrc
-                      return $ if readable permissions
-                                   then "--noplugin -u '" ++ vimrc ++ "' "
-                                   else ""
-                  else return ""
-          runVim src dst hsrc hdst = do
-              hPutStr hsrc contents
-              mapM_ hClose [hsrc, hdst]
-              vimrc <- vimrcM
-              {- vim must think that it was launched from a terminal, otherwise
-               - it won't load its usual environment and the syntax engine! -}
-              hin <- openFile "/dev/tty" ReadMode
-              (_, Just hout, _, handle) <- createProcess (shell $
-                  "vim -Nen " ++ cmds ++ vimrc ++ colorscheme ++
-                  "-c 'set ft=" ++ ft ++ " | " ++ vimhlcmd ++ "' " ++
-                  "-c 'w! " ++ dst ++ "' -c 'qa!' " ++ src)
-                  {std_in = UseHandle hin, std_out = CreatePipe}
-              waitForProcess handle
-              mapM_ hClose [hin, hout]
-      block <- withSystemTempFile "_vimhl_src." $
-                  \src hsrc -> withSystemTempFile "_vimhl_dst." $
-                      \dst hdst -> do
-                          runVim src dst hsrc hdst
-                          readFile dst
-      return $ RawBlock fm block
-  | otherwise = return cb
-  where namevals' = map (first $ map toLower) namevals
+    | lookup "hl" namevals' == Just "vim" && fmt `elem` ["html", "latex"] = do
+        let vimhlcmd =
+                unwords [cmd fmt, nmb]
+                where cmd "html"  = "MakeHtmlCodeHighlight"
+                      cmd "latex" = "MakeTexCodeHighlight"
+                      nmb | "numberLines" `elem` cls =
+                              fromMaybe "-1" $ lookup "startfrom" namevals'
+                          | otherwise = ""
+            colorscheme =
+                case lookup "colorscheme" namevals' of
+                    Nothing -> ""
+                    Just val -> "-c 'let g:PhColorscheme = \"" ++ val ++ "\"'"
+            cmds =
+                case lookup "vars" namevals' of
+                    Nothing -> ""
+                    Just val ->
+                        unwords $ map (cmd . flag) $ filter (not . null) $
+                            map (splitRegex regex') $ splitRegex regex val
+                        where cmd (x:y:_) =
+                                  "--cmd 'let g:" ++ x ++ " = \"" ++ y ++ "\"'"
+                              flag [x]    = [x, "1"]
+                              flag x      = x
+                              regex       = mkRegex "[[:space:]]*,[[:space:]]*"
+                              regex'      = mkRegex "[[:space:]]*=[[:space:]]*"
+            vimrcM = do
+                home <- getHomeDirectory
+                let vimrc = home `combine` ".vimrc.pandoc"
+                exists <- doesFileExist vimrc
+                if exists
+                    then do
+                        permissions <- getPermissions vimrc
+                        return $ if readable permissions
+                                     then "--noplugin -u '" ++ vimrc ++ "'"
+                                     else ""
+                    else return ""
+            runVim src dst hsrc hdst = do
+                hPutStr hsrc contents
+                mapM_ hClose [hsrc, hdst]
+                vimrc <- vimrcM
+                {- vim must think that it was launched from a terminal,
+                 - otherwise it won't load its usual environment and the
+                 - syntax engine! -}
+                hin <- openFile "/dev/tty" ReadMode
+                (_, Just hout, _, handle) <- createProcess (shell $ unwords
+                    ["vim -Nen", cmds, vimrc, colorscheme, "-c 'set ft=" ++ ft,
+                     "|", vimhlcmd ++ "' -c 'w!", dst ++ "' -c 'qa!'", src])
+                    {std_in = UseHandle hin, std_out = CreatePipe}
+                waitForProcess handle
+                mapM_ hClose [hin, hout]
+        block <- withSystemTempFile "_vimhl_src." $
+                    \src hsrc -> withSystemTempFile "_vimhl_dst." $
+                        \dst hdst -> do
+                            runVim src dst hsrc hdst
+                            readFile dst
+        return $ RawBlock fm block
+    | otherwise = return cb
+    where namevals' = map (first $ map toLower) namevals
 vimHl _ cb = return cb
 
 main :: IO ()
